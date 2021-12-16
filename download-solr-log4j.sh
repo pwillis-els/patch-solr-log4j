d91a567c69ca9303dcb058b36906fb898539752b
Peter Willis <31324861+peterwwillis@users.noreply.github.com>
2021-12-16 14:09:09 -0500

    Found the correct archive for solr

--- download-solr-log4j.sh
+++ download-solr-log4j.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env sh
 set -e -u
 
+# NOTE: 
+
 # Set these as environment variables in your shell to override them here
 SOLR_VERSION="${SOLR_VERSION:-7.7.3}"
 LOG4J_VERSION="${LOG4J_VERSION:-2.16.0}"
@@ -16,7 +18,7 @@ tar -xvzf "apache-log4j-${LOG4J_VERSION}-bin.tar.gz"
 [ -e "solr-${SOLR_VERSION}.tgz" ] || \
     curl -fSL \
         -o "solr-${SOLR_VERSION}.tgz" \
-        "https://dlcdn.apache.org/lucene/solr/${SOLR_VERSION}/solr-${SOLR_VERSION}.tgz"
+        "https://archive.apache.org/dist/lucene/solr/${SOLR_VERSION}/solr-${SOLR_VERSION}.tgz"
 
 rm -rf "solr-${SOLR_VERSION}/"
 tar -xvzf "solr-${SOLR_VERSION}.tgz"
